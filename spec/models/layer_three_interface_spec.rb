c19d2990e09b5ea62f2d89321902d933287d9bb5
Bryan T. Richardson <scada@sandia.gov>
2010-05-19 16:13:05 -0600

    basic tests for layer 3 interface

--- spec/models/layer_three_interface_spec.rb
+++ spec/models/layer_three_interface_spec.rb
@@ -40,3 +40,70 @@ require 'spec/spec_helper'
 #
 # Destoying a model should also destroy any and all IP interface models
 # associated with the model.
+
+describe Antfarm::Model::LayerThreeInterface, '#create' do
+  it 'should fail if no certainty factor exists' do
+    iface = Antfarm::Model::LayerThreeInterface.create :certainty_factor => nil
+    iface.valid?.should == false
+    iface.saved?.should == false
+  end
+
+  it 'should clamp the certainty factor between the predefined PROVEN values' do
+    iface = Antfarm::Model::LayerThreeInterface.create :certainty_factor => 0.5
+    iface.certainty_factor.should == 0.5
+    iface = Antfarm::Model::LayerThreeInterface.create :certainty_factor => -1.5
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_FALSE
+    iface = Antfarm::Model::LayerThreeInterface.create :certainty_factor => 1.5
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_TRUE
+  end
+
+  it 'should create a new layer 2 interface' do
+    count = Antfarm::Model::LayerTwoInterface.all.length
+    iface = Antfarm::Model::LayerThreeInterface.create
+    iface.layer_two_interface.should_not == nil
+    Antfarm::Model::LayerTwoInterface.all.length.should == count + 1
+    iface.layer_two_interface.should == Antfarm::Model::LayerTwoInterface.all.last
+  end
+
+  it 'should use a given layer 2 interface' do
+    l2_iface = Antfarm::Model::LayerTwoInterface.create
+    count    = Antfarm::Model::LayerTwoInterface.all.length
+    l3_iface = Antfarm::Model::LayerThreeInterface.create :layer_two_interface => l2_iface
+    l3_iface.layer_two_interface.should_not == nil
+    Antfarm::Model::LayerTwoInterface.all.length.should == count
+    l3_iface.layer_two_interface.should == l2_iface
+  end
+end
+
+describe Antfarm::Model::LayerThreeInterface, '#save' do
+  it 'should fail if no certainty factor exists' do
+    iface = Antfarm::Model::LayerThreeInterface.create
+    iface.certainty_factor = nil
+    iface.valid?.should == false
+    iface.errors.length.should == 1
+    iface.certainty_factor.should != nil
+  end
+
+  it 'should clamp the certainty factor between the predefined PROVEN values' do
+    iface = Antfarm::Model::LayerThreeInterface.create
+    iface.saved?.should == true
+    iface.certainty_factor = 0.5
+    iface.save
+    iface.certainty_factor.should == 0.5
+    iface.certainty_factor = -1.5
+    iface.save
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_FALSE
+    iface.certainty_factor = 1.5
+    iface.save
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_TRUE
+  end
+
+  it 'should not create a new layer 2 interface model' do
+    iface = Antfarm::Model::LayerThreeInterface.create
+    id    = iface.layer_two_interface.id
+    iface.certainty_factor = 0.5
+    iface.save
+    iface.certainty_factor.should == 0.5
+    iface.layer_two_interface.id.should == id
+  end
+end
