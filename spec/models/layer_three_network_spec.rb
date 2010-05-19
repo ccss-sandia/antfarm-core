f5a682ed6b66db2da2b3d47544cad0e000fba5c2
Bryan T. Richardson <scada@sandia.gov>
2010-05-19 16:19:19 -0600

    basic tests for layer 3 network

--- spec/models/layer_three_network_spec.rb
+++ spec/models/layer_three_network_spec.rb
@@ -28,3 +28,44 @@ require 'spec/spec_helper'
 # supplied, the layer 3 network should be returned. Otherwise, a new IP network
 # should be created (which will automatically cause a new layer 3 network to be
 # created) and the associated layer 3 network should be returned.
+
+describe Antfarm::Model::LayerThreeNetwork, '#create' do
+  it 'should fail if no certainty factor exists' do
+    iface = Antfarm::Model::LayerThreeNetwork.create :certainty_factor => nil
+    iface.valid?.should == false
+    iface.saved?.should == false
+  end
+
+  it 'should clamp the certainty factor between the predefined PROVEN values' do
+    iface = Antfarm::Model::LayerThreeNetwork.create :certainty_factor => 0.5
+    iface.certainty_factor.should == 0.5
+    iface = Antfarm::Model::LayerThreeNetwork.create :certainty_factor => -1.5
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_FALSE
+    iface = Antfarm::Model::LayerThreeNetwork.create :certainty_factor => 1.5
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_TRUE
+  end
+end
+
+describe Antfarm::Model::LayerThreeNetwork, '#save' do
+  it 'should fail if no certainty factor exists' do
+    iface = Antfarm::Model::LayerThreeNetwork.create
+    iface.certainty_factor = nil
+    iface.valid?.should == false
+    iface.errors.length.should == 1
+    iface.certainty_factor.should != nil
+  end
+
+  it 'should clamp the certainty factor between the predefined PROVEN values' do
+    iface = Antfarm::Model::LayerThreeNetwork.create
+    iface.saved?.should == true
+    iface.certainty_factor = 0.5
+    iface.save
+    iface.certainty_factor.should == 0.5
+    iface.certainty_factor = -1.5
+    iface.save
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_FALSE
+    iface.certainty_factor = 1.5
+    iface.save
+    iface.certainty_factor.should == Antfarm::Helpers::CF_PROVEN_TRUE
+  end
+end
