f03ffb636f53b4af30082801790b5f2f2ade94dc
Bryan T. Richardson <scada@sandia.gov>
2010-05-19 20:08:48 -0600

    basic tests for IP network

--- spec/models/ip_network_spec.rb
+++ spec/models/ip_network_spec.rb
@@ -4,6 +4,11 @@ require 'spec/spec_helper'
 #
 # Create
 #
+# Creating a model should fail if no address is present.
+#
+# Creating a model should fail if the address present
+# doesn't match the correct format for an IP network.
+#
 # Creating a model should also create a new associated layer 3 network model
 # if one is not supplied. If layer 3 network information is supplied, the
 # layer 3 network model created should be initialized with the supplied
@@ -12,5 +17,54 @@ require 'spec/spec_helper'
 #
 # Save
 #
-# Saving a model should not modify any information on any models associated
-# with the model.
+# Saving a model should require the presence of an ip
+# network address and the address provided should match
+# the correct format for an IP network.
+
+describe Antfarm::Model::IpNetwork, '#create' do
+  it 'should fail if no address exists' do
+    network = Antfarm::Model::IpNetwork.create
+    network.valid?.should == false
+    network.saved?.should == false
+  end
+
+  it 'should fail if the address is not in IP network format' do
+    network = Antfarm::Model::IpNetwork.create :address => 'foo.bar'
+    network.valid?.should == false
+    network.saved?.should == false
+  end
+
+  it 'should create a new layer three network' do
+    count   = Antfarm::Model::LayerThreeNetwork.all.length
+    network = Antfarm::Model::IpNetwork.create :address => '192.168.101.0/24'
+    network.layer_three_network.should_not == nil
+    Antfarm::Model::LayerThreeNetwork.all.length.should == count + 1
+    network.layer_three_network.should == Antfarm::Model::LayerThreeNetwork.all.last
+  end
+
+  it 'should use a given certainty factor when creating a new layer three network' do
+    count   = Antfarm::Model::LayerThreeNetwork.all.length
+    network = Antfarm::Model::IpNetwork.new
+    network.address = '192.168.101.0/24'
+    network.layer_three_network = { :certainty_factor => 0.5 }
+    network.save
+    network.layer_three_network.should_not == nil
+    Antfarm::Model::LayerThreeNetwork.all.length.should == count + 1
+    network.layer_three_network.should == Antfarm::Model::LayerThreeNetwork.all.last
+    network.layer_three_network.certainty_factor.should == 0.5
+  end
+
+  # TODO: would this EVER be necessary?! <scrapcoder>
+  it 'should use a given layer three network' do
+    count  = Antfarm::Model::LayerThreeNetwork.all.length
+    l3_net = Antfarm::Model::LayerThreeNetwork.create
+    Antfarm::Model::LayerThreeNetwork.all.length.should == count + 1
+    Antfarm::Model::LayerThreeNetwork.all.last.should == l3_net
+    network = Antfarm::Model::IpNetwork.new
+    network.address = '192.168.101.0/24'
+    network.layer_three_network = l3_net
+    network.save
+    network.layer_three_network.should_not == nil
+    network.layer_three_network.should == l3_net
+  end
+end
