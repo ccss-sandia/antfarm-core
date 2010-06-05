c591a2af0ba3fa1a1fc4770189b50d49420793ac
Bryan T. Richardson <scada@sandia.gov>
2010-06-04 19:31:10 -0600

    moved db actions to framework and removed command-line db actions

--- lib/antfarm/framework.rb
+++ lib/antfarm/framework.rb
@@ -1,8 +1,5 @@
 require 'antfarm-plugins'
 
-require 'antfarm/database_manager'
-require 'antfarm-plugins'
-
 module Antfarm
   class Framework
     attr_reader :plugin
@@ -13,8 +10,80 @@ module Antfarm
       return @plugins ||= Antfarm::Plugin.load
     end
 
-    def db(args)
-      Antfarm::DatabaseManager.new(args)
+    def clean_db
+      message = "Deleting #{ANTFARM_ENV} log file..."
+      Antfarm::Helpers.output message
+      Antfarm::Helpers.log :info, message
+
+      FileUtils.rm "#{Antfarm::Helpers.log_file(ANTFARM_ENV)}" if File.exists?(Antfarm::Helpers.log_file(ANTFARM_ENV))
+
+      message = "Deleted #{ANTFARM_ENV} log file successfully."
+      Antfarm::Helpers.output message
+      Antfarm::Helpers.log :info, message
+
+      config = YAML::load(IO.read(Antfarm::Helpers.defaults_file))
+
+      # TODO <scrapcoder>: can this stuff be done using the postgres gem instead?
+      if config && config[ANTFARM_ENV] && config[ANTFARM_ENV]['adapter'] == 'postgres'
+        message = "Dropping PostgreSQL #{ANTFARM_ENV} database..."
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+
+        exec "dropdb #{ANTFARM_ENV}"
+
+        message = "Dropped PostgreSQL #{ANTFARM_ENV} database successfully."
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+      else
+        message = "Dropping SQLite3 #{ANTFARM_ENV} database..."
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+
+        FileUtils.rm "#{Antfarm::Helpers.db_file(ANTFARM_ENV)}" if File.exists?(Antfarm::Helpers.db_file(ANTFARM_ENV))
+
+        message = "Dropped SQLite3 #{ANTFARM_ENV} database successfully."
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+      end
+    end
+
+    # Creates a new database and schema file.  The location of the newly created
+    # database file is set in the initializer class via the user
+    # configuration hash, and is based on the current ANTFARM environment.
+    def migrate_db
+      begin
+        message = 'Migrating database...'
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+
+        DataMapper.auto_upgrade!
+
+        message = 'Database successfully migrated.'
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+      rescue => e # TODO: better error catching - not EVERY error is PostreSQL...
+        message = "No PostgreSQL database exists for the #{ANTFARM_ENV} environment. Creating PostgreSQL database..."
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+
+        exec "createdb #{ANTFARM_ENV}"
+
+        message = 'PostgreSQL database for this environment created. Continuing on with migration.'
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+
+        DataMapper.auto_upgrade!
+
+        message = 'Database successfully migrated.'
+        Antfarm::Helpers.output message
+        Antfarm::Helpers.log :info, message
+      end
+    end
+
+    # Forces a destructive migration
+    def reset_db
+      clean_db
+      migrate_db
     end
 
     def use(plugin)
@@ -28,6 +97,7 @@ module Antfarm
 
     def run(options = Hash.new)
       raise Antfarm::AntfarmError, 'No plugin loaded' unless @plugin
+
       if @plugin.options.nil?
         @plugin.run
       else
